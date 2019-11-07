e54d7cc67222881def47fe7fd108edd5905e3fa3
love190love <love190love@yahoo.com.tw>
2019-11-07 23:22:07 +0800

    up

--- HomeWork/src/main/java/com/example/homework/HomeWork02.java
+++ HomeWork/src/main/java/com/example/homework/HomeWork02.java
@@ -2,18 +2,18 @@ package com.example.homework;
 
 public class HomeWork02 {
     public  static  void main(String[] avrg) {
-        java.util.Scanner scanner = new
+        java.util.Scanner scanner= new
                 java.util.Scanner(System.in);
 
         System.out.println("出生年(西元):");
-        int inp1 = scanner.nextInt();
+        int inp1=scanner.nextInt();
 
         System.out.println("出生月:");
-        int inp2 = scanner.nextInt();
+        int inp2=scanner.nextInt();
 
         System.out.println("出生日:");
-        int inp3 = scanner.nextInt();
+        int inp3=scanner.nextInt();
 
-        System.out.println("生日日期:" + inp1 + " " + inp2 + " " + inp3);
+        System.out.println("出生日期:"+inp1+" "+inp2+" "+inp3);
     }
 }
